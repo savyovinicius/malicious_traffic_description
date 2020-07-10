2586f9b8ae1ba132a87cc23b8511ce2237a94a26
labnet <savyovm@gmail.com>
2020-07-10 17:36:05 -0300

    data-loss prevention on flow counters

--- experiment/new_scripts/get_counters.sh
+++ experiment/new_scripts/get_counters.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo ovs-ofctl dump-flows br0 | grep n_packets=[^0] > ../new_counters/$1_$2_flows.txt
+sudo ovs-ofctl dump-flows br0 > ../new_counters/$1_$2_flows.txt
 scp mirai@192.168.56.3:/tmp/*.output ../new_counters
 mv ../new_counters/cnc.output ../new_counters/$1_$2_cnc.output
 mv ../new_counters/loader.output ../new_counters/$1_$2_loader.output
