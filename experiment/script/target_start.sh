68fa3fcbf52d7b7efa2a5e87536d385b528b9f16
Sávyo <savyovm@gmail.com>
2020-12-15 12:21:37 -0300

    rm old folders

--- experiment/script/target_start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-docker run --network none --name bb --rm -d -it mirai_target
-sudo ovs-docker add-port br0 eth1 bb --ipaddress="192.168.5.104/24" --gateway="192.168.5.1"
-docker exec -it bb /bin/sh
